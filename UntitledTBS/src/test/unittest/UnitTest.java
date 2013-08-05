407b3bb75e0c73007d4c8c5281959bcb796aa862
misthema <misthema@gmail.com>
2013-08-05 07:12:54 +0300

    Updated UnitTest.java

--- UntitledTBS/src/test/unittest/UnitTest.java
+++ UntitledTBS/src/test/unittest/UnitTest.java
@@ -1,32 +1,54 @@
 package test.unittest;
 
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
-public class UnitTest extends BasicGame {
+public class UnitTest extends BasicGame
+{
 
-	public UnitTest(String title) {
-		super(title);
-		// TODO Auto-generated constructor stub
+	public UnitTest(String title)
+	{
+		super( title );
+	}
+
+	public void main ( String[] args )
+	{
+		try
+		{
+			AppGameContainer app = new AppGameContainer( new UnitTest(
+			        "Unit test #1" ) );
+
+			app.setDisplayMode( 800, 600, false );
+			app.setTargetFrameRate( 60 );
+			app.setShowFPS( true );
+			app.start( );
+
+		} catch (Exception e)
+		{
+			e.printStackTrace( );
+		}
+
 	}
 
 	@Override
-	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
-		// TODO Auto-generated method stub
+	public void init ( GameContainer arg0 ) throws SlickException
+	{
 
 	}
 
 	@Override
-	public void init(GameContainer arg0) throws SlickException {
-		// TODO Auto-generated method stub
+	public void render ( GameContainer arg0, Graphics arg1 )
+	        throws SlickException
+	{
 
 	}
 
 	@Override
-	public void update(GameContainer arg0, int arg1) throws SlickException {
-		// TODO Auto-generated method stub
+	public void update ( GameContainer arg0, int arg1 ) throws SlickException
+	{
 
 	}
 
