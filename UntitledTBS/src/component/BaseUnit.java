b88f0b1ee442b805a414eab855e303db3a0ba95f
Jani Mustonen <janijohannes@kapsi.fi>
2013-08-05 09:22:11 +0300

    Incomplete - task : Finish BaseUnit
    - Continued on BaseUnit
    - System got mad, reinitializing.

--- UntitledTBS/src/component/BaseUnit.java
+++ UntitledTBS/src/component/BaseUnit.java
@@ -1,16 +1,94 @@
 package component;
 
 import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Transform;
 
-public class BaseUnit {
+/**
+ * The base unit with basic methods.
+ * 
+ * @author Jani
+ * 
+ */
+public class BaseUnit
+{
 
 	Vector2f position;
-	Vector2f destination;
-	
-	public BaseUnit( ) {
-		this.position = new Vector2f(0,0);
+	Image image;
+	Shape basehit;
+	float angle;
+
+	/**
+	 * Creates a new unit at the top left position with blank image.
+	 * 
+	 * @throws SlickException
+	 */
+	public BaseUnit() throws SlickException
+	{
+		this.init(new Image(2, 2),
+		          new Vector2f(0, 0),
+		          new Rectangle(0, 0, 1, 1),
+		          0.0f);
 	}
+
+	/**
+	 * Creates a new unit at the top left position. Loads image from given path.
+	 * 
+	 * @param uri
+	 *            The path to load the image from.
+	 * @throws SlickException
+	 */
+	public BaseUnit(String uri) throws SlickException
+	{
+		Image img = new Image(uri);
+		this.init(img,
+		          new Vector2f(0, 0),
+		          new Rectangle(0, 0, img.getWidth(), img.getHeight()),
+		          0.0f);
+	}
+
+	/**
+	 * Internal initializer for BaseUnit.
+	 * 
+	 * @param image
+	 *            The image used as the sprite of the unit.
+	 * @param pos
+	 *            Position the unit will start at.
+	 * @param hit
+	 *            Shape for hit detection.
+	 * @param angle
+	 *            Angle the unit will start at.
+	 */
+	private void init(Image image, Vector2f pos, Shape hit, float angle)
+	{
+		this.image = image;
+		this.position = pos;
+		this.basehit = hit;
+		this.angle = angle;
+
+		// Move the shape to corresponding location.
+		this.basehit.setLocation(pos.x + hit.getX(),
+		                         pos.y + hit.getY());
+	}
+
+	/**
+	 * Draw the unit at it's current position and rotation.
+	 */
+	public void draw()
+	{
+		image.draw(position.x,
+		           position.y);
+	}
+
+	/* ##### Getters ##### */
+	/* @formatter:off */
+
+	public Shape getHit() { return this.basehit; }
 	
-	
+	/* @formatter:on */
 
 }
--- UntitledTBS/src/test/unittest/UnitTest.java
+++ UntitledTBS/src/test/unittest/UnitTest.java
@@ -1,33 +1,59 @@
 package test.unittest;
 
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
-public class UnitTest extends BasicGame {
+import test1.Test1;
+import component.BaseUnit;
 
-	public UnitTest(String title) {
+public class UnitTest extends BasicGame
+{
+	BaseUnit unit;
+
+	public UnitTest(String title)
+	{
 		super(title);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
-		// TODO Auto-generated method stub
-
+	public void init(GameContainer gc) throws SlickException
+	{
+		this.unit = new BaseUnit("res/unittest/arrow.png");
 	}
 
 	@Override
-	public void init(GameContainer arg0) throws SlickException {
-		// TODO Auto-generated method stub
-
+	public void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		this.unit.draw();
 	}
 
 	@Override
-	public void update(GameContainer arg0, int arg1) throws SlickException {
-		// TODO Auto-generated method stub
+	public void update(GameContainer gc, int delta) throws SlickException
+	{
 
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			BasicGame game = new UnitTest("Unit test");
+
+			AppGameContainer app = new AppGameContainer(game);
+			app.setDisplayMode(800,
+			                   600,
+			                   false);
+			app.setShowFPS(true);
+			app.setTargetFrameRate(60);
+			app.start();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
